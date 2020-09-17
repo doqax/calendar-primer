75ace5c353e8631502f3f2bba64d25e488ed3082
Dollier <d.co@tutanota.com>
2020-09-17 02:32:42 +0200

    minor change to handle className

--- src/Calendar/CalendarMonth/Day.js
+++ src/Calendar/CalendarMonth/Day.js
@@ -4,18 +4,20 @@ import { Absolute, Text } from "@primer/components";
 import moment from "moment";
 import "./day.css";
 
+
 function Day({ calendar, currentDate, showDay }) {
   const [calendarState] = calendar;
   const { today, date } = calendarState;
   const isToday = moment(today).isSame(currentDate, "day");
   const isCurrentMonth = moment(currentDate).isSame(date, "month");
+  const isWeekend = moment(currentDate).format("d") == 0 || moment(currentDate).format("d") == 6; // 5 for isoDay
 
   const color = isToday ? "blue.4" : isCurrentMonth ? "gray.9" : "gray.4";
-  const bg = !isCurrentMonth ? "blanks" : isToday ? "today" : "";
+  const bg = !isCurrentMonth ? "blanks" : isWeekend ? " weekend" : "";
 
 
   return (
-    <div className={`calendarMonth--day ${bg}`}>
+    <div className={`calendarMonth--day ${bg} ${isToday ? " today" : ""}`}>
       <Absolute width="100%" m={0} right={0} top={0}>
         <Text as="p" my={1} mt={1} fontSize={14} p={0} textAlign="center" color={"gray.9"} fontWeight="bold">
           {showDay <= 6 && currentDate.format("ddd")}
